526905748ee8fce8a55ba64676df831cb86b6668
samjanderson <sam.andersonnn@live.ca>
2021-01-19 05:26:11 +0000

    added missing final redirect to long url step

--- express_server.js
+++ express_server.js
@@ -44,12 +44,26 @@ app.get("/urls/new", (req, res) => { //I think it says this needs to go above th
   res.render("urls_new");
 });
 
+// localhost:8080:/urls/hello/world
+// :shortURL = 'hello'
+// :otherThing = 'world'
+
+const myFunc = function(shortURL, otherThing) {
+  // do something with shortURL
+}
+
+//myFunc('hello', 'world')
+//myFunc(shortURL, otherThing) but the actual values are the req.params.otherThing and req.params.shortUrl
+
 app.get("/urls/:shortURL", (req, res) => {  //think of the : as a parameter denotes that shortURL is a dynamic parameter req.params is like saying what is in the URL it will be the 2xnb etc, object with keys you define in the HTML
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; //gets defined when you make a request by going into the website
   //console.log(templateVars)
-  console.log('hello')
+  console.log('hello') //writing this on the server this is all server, will run inside the terminal
   res.render("urls_show", templateVars); //res.render is like madlibs we call them templateVars because they are the variables that will end up in the template
-});
+}); ///res.render sends something back to the browser
+
+// localhost:8080/urls, POST
+//wendys/tim hortons same building is the server -> there is a valet and you tell them if you want to go to tims or wendys (like get and post here)
 
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console, this will hold the content
@@ -57,8 +71,14 @@ app.post("/urls", (req, res) => {
   urlDatabase[randomShortUrl] = req.body.longURL;
   // res.redirect(req.body.longURL); //redirect to the other route so the url/ short url like an edit page for the short URL status code of 301, 301 and 302 are redirects that are widely used
   // console.log(urlDatabase)
-  res.redirect(`/urls/${randomShortUrl}`)
+  res.redirect(`/urls/${randomShortUrl}`) // localhost:8080/urls/123abc
   // res.send("Ok");         // Respond with 'Ok' (we will replace this) sends an HTTP status of 200 and then in the body here it is sending ok
+  //redirect is going back to the browser
+});
+
+// localhost:8080/u/9sm5xK -> "http://www.google.com"
+app.get('/u/:shortURL', (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL])
 });
 
 //google.ca you make a browser request to google who sends back HTML to your browser
